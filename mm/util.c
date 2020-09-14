c3618512184c7670ea7e9ece445ea1d3d5ee9f80
Christian König <christian.koenig@amd.com>
2020-09-14 15:09:33 +0200

    mm: introduce vma_set_file function v5
    Add the new vma_set_file() function to allow changing
    vma->vm_file with the necessary refcount dance.
    
    v2: add more users of this.
    v3: add missing EXPORT_SYMBOL, rebase on mmap cleanup,
        add comments why we drop the reference on two occasions.
    v4: make it clear that changing an anonymous vma is illegal.
    v5: move vma_set_file to mm/util.c
    
    Change-Id: I1e8ae3ca30874e71d41ff531b46bb7ed02df247f
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch> (v2)
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Acked-by: Andrew Morton <akpm@linux-foundation.org>
    Link: https://patchwork.freedesktop.org/patch/399360/
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: iDeadXS <datarafi43@gmail.com>

--- mm/util.c
+++ mm/util.c
@@ -290,6 +290,18 @@ int vma_is_stack_for_current(struct vm_area_struct *vma)
 	return (vma->vm_start <= KSTK_ESP(t) && vma->vm_end >= KSTK_ESP(t));
 }
 
+/*
+ * Change backing file, only valid to use during initial VMA setup.
+ */
+void vma_set_file(struct vm_area_struct *vma, struct file *file)
+{
+	/* Changing an anonymous vma with this is illegal */
+	get_file(file);
+	swap(vma->vm_file, file);
+	fput(file);
+}
+EXPORT_SYMBOL(vma_set_file);
+
 /**
  * randomize_page - Generate a random, page aligned address
  * @start:	The smallest acceptable address the caller will take.
