39132cdbc436905b49fff014163330f9ca30169d
Dietmar Eggemann <dietmar.eggemann@arm.com>
2022-07-29 13:13:03 +0200

    sched/core: Introduce sched_asym_cpucap_active()
    Create an inline helper for conditional code to be only executed on
    asymmetric CPU capacity systems. This makes these (currently ~10 and
    future) conditions a lot more readable.
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20220729111305.1275158-2-dietmar.eggemann@arm.com
    Signed-off-by: Muhammad Guntur <mochtur@gmail.com>
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>
    Signed-off-by: iDeadXS <datarafi43@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5082,9 +5082,9 @@ static inline void adjust_cpus_for_packing(struct task_struct *p,
 
 static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 {
-        int cpu = cpu_of(rq);
+    int cpu = cpu_of(rq);
 
-	if (!static_branch_unlikely(&sched_asym_cpucapacity))
+	if (!sched_asym_cpucap_active())
 		return;
 
 	/*
@@ -7534,7 +7534,7 @@ static inline bool asym_fits_cpu(unsigned long util,
 				 int cpu)
 {
 
-	if (static_branch_unlikely(&sched_asym_cpucapacity))
+	if (sched_asym_cpucap_active())
 		/*
 		 * Return true only if the cpu fully fits the task requirements
 		 * which include the utilization and the performance hints.
@@ -7558,7 +7558,7 @@ static int select_idle_sibling(struct task_struct *p, int prev, int target)
 	 * On asymmetric system, update task utilization because we will check
 	 * that the task fits with cpu's capacity.
 	 */
-	if (static_branch_unlikely(&sched_asym_cpucapacity)) {
+	if (sched_asym_cpucap_active()) {
 		sync_entity_load_avg(&p->se);
 		task_util = task_util_est(p);
 		util_min = uclamp_eff_value(p, UCLAMP_MIN);
@@ -7598,7 +7598,7 @@ static int select_idle_sibling(struct task_struct *p, int prev, int target)
 	 * For asymmetric CPU capacity systems, our domain of interest is
 	 * sd_asym_cpucapacity rather than sd_llc.
 	 */
-	if (static_branch_unlikely(&sched_asym_cpucapacity)) {
+	if (sched_asym_cpucap_active()) {
 		sd = rcu_dereference(per_cpu(sd_asym_cpucapacity, target));
 		/*
 		 * On an asymmetric CPU capacity system where an exclusive
