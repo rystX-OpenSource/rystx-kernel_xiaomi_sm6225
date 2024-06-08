7d6e2b68e1c89ec3755b3b71dc27ad969e4c236e
Alexander Winkowski <dereference23@outlook.com>
2024-06-08 17:49:58 +0000

    lpm-levels: Expose `sleep_disabled`
    Change-Id: Ide920ba562d096cd7d29efc3d09bcb71a7ecbdcf
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -97,6 +97,12 @@ module_param_named(print_parsed_dt, print_parsed_dt, bool, 0664);
 static bool sleep_disabled;
 module_param_named(sleep_disabled, sleep_disabled, bool, 0664);
 
+bool lpm_sleep_disabled(void)
+{
+	return sleep_disabled;
+}
+EXPORT_SYMBOL(lpm_sleep_disabled);
+
 /**
  * msm_cpuidle_get_deep_idle_latency - Get deep idle latency value
  *
