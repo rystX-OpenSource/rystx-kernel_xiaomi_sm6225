6336401013f8b088e9a2256475806ede6cc67784
Sergey Senozhatsky <senozhatsky@chromium.org>
2024-09-17 11:09:06 +0900

    zram: introduce ZRAM_PP_SLOT flag
    Patch series "zram: optimal post-processing target selection", v5.
    
    Problem:
    --------
    Both recompression and writeback perform a very simple linear scan of all
    zram slots in search for post-processing (writeback or recompress)
    candidate slots.  This often means that we pick the worst candidate for pp
    (post-processing), e.g.  a 48 bytes object for writeback, which is nearly
    useless, because it only releases 48 bytes from zsmalloc pool, but
    consumes an entire 4K slot in the backing device.  Similarly,
    recompression of an 48 bytes objects is unlikely to save more memory that
    recompression of a 3000 bytes object.  Both recompression and writeback
    consume constrained resources (CPU time, batter, backing device storage
    space) and quite often have a (daily) limit on the number of items they
    post-process, so we should utilize those constrained resources in the most
    optimal way.
    
    Solution:
    ---------
    This patch reworks the way we select pp targets.  We, quite clearly, want
    to sort all the candidates and always pick the largest, be it
    recompression or writeback.  Especially for writeback, because the larger
    object we writeback the more memory we release.  This series introduces
    concept of pp buckets and pp scan/selection.
    
    The scan step is a simple iteration over all zram->table entries, just
    like what we currently do, but we don't post-process a candidate slot
    immediately.  Instead we assign it to a PP (post-processing) bucket.  PP
    bucket is, basically, a list which holds pp candidate slots that belong to
    the same size class.  PP buckets are 64 bytes apart, slots are not
    strictly sorted within a bucket there is a 64 bytes variance.
    
    The select step simply iterates over pp buckets from highest to lowest and
    picks all candidate slots a particular buckets contains.  So this gives us
    sorted candidates (in linear time) and allows us to select most optimal
    (largest) candidates for post-processing first.
    
    This patch (of 7):
    
    This flag indicates that the slot was selected as a candidate slot for
    post-processing (pp) and was assigned to a pp bucket.  It does not
    necessarily mean that the slot is currently under post-processing, but may
    mean so.  The slot can loose its PP_SLOT flag, while still being in the
    pp-bucket, if it's accessed or slot_free-ed.
    
    Link: https://lkml.kernel.org/r/20240917021020.883356-1-senozhatsky@chromium.org
    Link: https://lkml.kernel.org/r/20240917021020.883356-2-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -797,6 +797,7 @@ static void zram_debugfs_destroy(void)
 static void zram_accessed(struct zram *zram, u32 index)
 {
 	zram_clear_flag(zram, index, ZRAM_IDLE);
+	zram_clear_flag(zram, index, ZRAM_PP_SLOT);
 	zram->table[index].ac_time = ktime_get_boottime();
 }
 
@@ -1324,6 +1325,7 @@ static void zram_free_page(struct zram *zram, size_t index)
 		zram_clear_flag(zram, index, ZRAM_INCOMPRESSIBLE);
 
 	zram_set_priority(zram, index, 0);
+	zram_clear_flag(zram, index, ZRAM_PP_SLOT);
 
 	if (zram_test_flag(zram, index, ZRAM_WB)) {
 		zram_clear_flag(zram, index, ZRAM_WB);
