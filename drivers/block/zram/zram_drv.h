6d1088cd800bc5d174e1e5f1b57863034c77f96a
Juhyung Park <qkrwngud825@gmail.com>
2025-05-14 20:59:51 +0300

    zram: backport from 95848dcb9d67 Backport zram from commit 95848dcb9d67 ("zram: take device and not only bvec offset into account").
    [Tashar02]: Backport to msm-5.4 and revert the following commits for
    compatibility:
    
     - 95848dc ("zram: take device and not only bvec offset into account")
     - 82ca875 ("zram: refactor highlevel read and write handling")
     - af8b04c ("zram: simplify bvec iteration in __zram_make_request")
     - d7614e4 ("zram: nvdimm: use bio_{start,end}_io_acct and disk_{start,end}_io_acct")
     - 659e56b ("block: add a new revalidate_disk_size helper")
     - c62b37d ("block: move ->make_request_fn to struct block_device_operations")
     - 3d745ea ("block: simplify queue allocation")
     - 1cb039f ("bdi: replace BDI_CAP_STABLE_WRITES with a queue and a sb flag")
     - 86947df ("block: Change the type of the last .rw_page() argument")
    
    And retain the following commits:
    
     - a96ae75 ("zram: allow zram to allocate CMA pages")
     - 8b04672 ("mm: direct previous __GFP_CMA allocations to offlinable memory")
    
    Co-authored-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: iDeadXS <datarafi43@gmail.com>

--- drivers/block/zram/zram_drv.h
+++ drivers/block/zram/zram_drv.h
@@ -42,6 +42,9 @@
  */
 #define ZRAM_FLAG_SHIFT (PAGE_SHIFT + 1)
 
+/* Only 2 bits are allowed for comp priority index */
+#define ZRAM_COMP_PRIORITY_MASK	0x3
+
 /* Flags for zram pages (table[page_no].flags) */
 enum zram_pageflags {
 	/* zram slot is locked */
@@ -49,7 +52,12 @@ enum zram_pageflags {
 	ZRAM_SAME,	/* Page consists the same element */
 	ZRAM_WB,	/* page is stored on backing_device */
 	ZRAM_UNDER_WB,	/* page is under writeback */
+	ZRAM_HUGE,	/* Incompressible page */
 	ZRAM_IDLE,	/* not accessed page since last idle marking */
+	ZRAM_INCOMPRESSIBLE, /* none of the algorithms could compress it */
+
+	ZRAM_COMP_PRIORITY_BIT1, /* First bit of comp priority index */
+	ZRAM_COMP_PRIORITY_BIT2, /* Second bit of comp priority index */
 
 	__NR_ZRAM_PAGEFLAGS,
 };
@@ -80,9 +88,10 @@ struct zram_stats {
 	atomic64_t num_writes;	/* --do-- */
 	atomic64_t failed_reads;	/* can happen when memory is too low */
 	atomic64_t failed_writes;	/* can happen when memory is too low */
-	atomic64_t invalid_io;	/* non-page-aligned I/O requests */
 	atomic64_t notify_free;	/* no. of swap slot free notifications */
 	atomic64_t same_pages;		/* no. of same element filled pages */
+	atomic64_t huge_pages;		/* no. of huge pages */
+	atomic64_t huge_pages_since;	/* no. of huge pages since zram set up */
 	atomic64_t pages_stored;	/* no. of pages currently stored */
 	atomic_long_t max_used_pages;	/* no. of maximum pages stored */
 	atomic64_t writestall;		/* no. of write slow paths */
@@ -104,10 +113,20 @@ struct zram_hash {
 	struct rb_root rb_root;
 };
 
+#ifdef CONFIG_ZRAM_MULTI_COMP
+#define ZRAM_PRIMARY_COMP	0U
+#define ZRAM_SECONDARY_COMP	1U
+#define ZRAM_MAX_COMPS	4U
+#else
+#define ZRAM_PRIMARY_COMP	0U
+#define ZRAM_SECONDARY_COMP	0U
+#define ZRAM_MAX_COMPS	1U
+#endif
+
 struct zram {
 	struct zram_table_entry *table;
 	struct zs_pool *mem_pool;
-	struct zcomp *comp;
+	struct zcomp *comps[ZRAM_MAX_COMPS];
 	struct gendisk *disk;
 	struct zram_hash *hash;
 	size_t hash_size;
@@ -124,7 +143,8 @@ struct zram {
 	 * we can store in a disk.
 	 */
 	u64 disksize;	/* bytes */
-	char compressor[CRYPTO_MAX_ALG_NAME];
+	const char *comp_algs[ZRAM_MAX_COMPS];
+	s8 num_active_comps;
 	/*
 	 * zram is claimed so open request will be failed
 	 */
