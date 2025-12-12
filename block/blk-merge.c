f2515ff1f2765f0e38dc848cbe4d0a1d03aed559
iDeadXS <datarafi43@gmail.com>
2025-12-13 05:00:22 +0700

    BACKPORT: blk: Fix lock inversion between ioc lock and bfqd lock
    based on https://github.com/torvalds/linux/commit/fd2ef39cc9a6b9c4c41864ac506906c52f94b06a

--- block/blk-merge.c
+++ block/blk-merge.c
@@ -800,20 +800,7 @@ struct request *attempt_front_merge(struct request_queue *q, struct request *rq)
 int blk_attempt_req_merge(struct request_queue *q, struct request *rq,
 			  struct request *next)
 {
-	struct elevator_queue *e = q->elevator;
-	struct request *free;
-
-	if (!e->uses_mq && e->type->ops.sq.elevator_allow_rq_merge_fn)
-		if (!e->type->ops.sq.elevator_allow_rq_merge_fn(q, rq, next))
-			return 0;
-
-	free = attempt_merge(q, rq, next);
-	if (free) {
-		__blk_put_request(q, free);
-		return 1;
-	}
-
-	return 0;
+	return attempt_merge(q, rq, next);
 }
 
 bool blk_rq_merge_ok(struct request *rq, struct bio *bio)
