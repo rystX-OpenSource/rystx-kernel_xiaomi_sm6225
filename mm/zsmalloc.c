053bbafcfd247a0cd1eb73a33b1a1367182163b4
alternoegraha <noegrahachan@gmail.com>
2025-12-26 21:40:10 +0700

    Merge tag 'v4.19.325-cip124' of https://git.kernel.org/pub/scm/linux/kernel/git/cip/linux-cip into ye
    version 4.19.325-cip124

--- mm/zsmalloc.c
+++ mm/zsmalloc.c
@@ -1090,7 +1090,9 @@ static struct zspage *alloc_zspage(struct zs_pool *pool,
 	if (!zspage)
 		return NULL;
 
-	memset(zspage, 0, sizeof(struct zspage));
+	if (!IS_ENABLED(CONFIG_COMPACTION))
+		gfp &= ~__GFP_MOVABLE;
+
 	zspage->magic = ZSPAGE_MAGIC;
 	migrate_lock_init(zspage);
 
