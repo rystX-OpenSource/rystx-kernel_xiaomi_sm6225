67881e967b5fd57de3f68f16ba50c7cfb3337946
alternoegraha <noegrahachan@gmail.com>
2026-04-03 20:18:51 +0700

    simple_lmk: adapt minfree

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -491,14 +491,14 @@ static int simple_lmk_init_set(const char *val, const struct kernel_param *kp)
 	}
 
 	si_meminfo(&i);
-	if (i.totalram << (PAGE_SHIFT-10) > 4096ull * 1024) {
-	  // from - phone-xhdpi-6144-dalvik-heap.mk
-	  slmk_minfree = 125;
-	  slmk_timeout = 160;
+	if (i.totalram << (PAGE_SHIFT-10) > 3072ull * 1024) {
+	  // 4GB+ variant
+	  slmk_minfree = 128;
+	  slmk_timeout = 200;
 	} else {
-	  // from - phone-xhdpi-4096-dalvik-heap.mk
-	  slmk_minfree = 155;
-	  slmk_timeout = 145;
+	  // 3GB or lower
+	  slmk_minfree = 256;
+	  slmk_timeout = 200;
 	}
 
 	return 0;
