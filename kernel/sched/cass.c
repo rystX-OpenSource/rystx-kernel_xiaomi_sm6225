3520b4637f8b1a9a4b83778a29c6d73dfa929709
John Galt <johngaltfirstrun@gmail.com>
2026-07-09 18:27:17 -0400

    sched/cass: Reorder for peak performance without standard eff cost
    This targets full load scenarios only due to our other prime avoidance cases, when
    a task can only fit on prime due to full load.
    
    Geekbench multi-core averages went from ~6500s to ~6800s with this
    change (with a devicetrees change getting it to ~7000s).
    
    Change-Id: If9df94b02671d64690c78d10b18925e5981f1311
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: iDeadXS <datarafi43@gmail.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -32,6 +32,7 @@ struct cass_cpu_cand {
 	unsigned long cap_max;
 	unsigned long cap_no_therm;
 	unsigned long cap_orig;
+	unsigned long cap_actual;
 	unsigned long therm_press;
 	unsigned long eff_util;
 	unsigned long hard_util;
@@ -106,6 +107,10 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 		     fits_capacity(p_util, b->cap_max)))
 		goto done;
 
+	/* Prefer the CPU with higher actual capacity */
+	if (cass_cmp(a->cap_actual, b->cap_actual))
+		goto done;
+
 	/* Prefer the CPU with lower relative utilization */
 	if (cass_cmp(b->util, a->util))
 		goto done;
@@ -151,10 +156,6 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 
 	/* Prefer the current CPU for sync wakes */
 	if (sync && (cass_eq(a->cpu, this_cpu) || !cass_cmp(b->cpu, this_cpu)))
-		goto done;
-
-	/* Prefer the CPU with higher capacity */
-	if (cass_cmp(a->cap, b->cap))
 		goto done;
 
 	/* Prefer the previous CPU */
@@ -237,6 +238,9 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 			curr->therm_press = 0;
 		else
 			curr->therm_press = curr->cap_orig - curr->cap_max;
+
+		/* Actual available capacity after thermal pressure */
+		curr->cap_actual = curr->cap_orig - curr->therm_press;
 
 		/* Prefer the CPU that more closely meets the uclamp minimum */
 		if (curr->cap_max < uc_min && curr->cap_max < best->cap_max)
