1098cf603f7ccc02c295cd63a740d90555936b47
John Galt <johngaltfirstrun@gmail.com>
2026-07-21 17:22:21 -0400

    binder: Bypass priority inheritance cap for critical task senders
    When a critical task sends a binder tx, the thread priority can be downgraded by
    two mechanisms which harm round trip latency.
    
    This logic is modeled off the vendor hooks of trace_android_vh_binder_set_priority
    and trace_android_vh_binder_priority_skip.
    
    The task_is_critical is effectively the same as is used in mm/vmscan.c and
    mm/readahead.c for assessing critical tasks and adapting vendor hook logic.
    
    [Tashar02: Include oom.h for task_is_critical() access]
    Change-Id: Iaa72cf653c754e7e34b14cd454ae69c7f7357ccf
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>
    Signed-off-by: iDeadXS <datarafi43@gmail.com>
    
    # Conflicts:
    #	drivers/android/binder.c

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -56,6 +56,7 @@
 #include <linux/rbtree.h>
 #include <linux/sched/signal.h>
 #include <linux/sched/mm.h>
+#include <linux/oom.h>
 #include <linux/seq_file.h>
 #include <linux/string.h>
 #include <linux/uaccess.h>
@@ -860,7 +861,8 @@ static void binder_transaction_priority(struct binder_thread *thread,
 
 	t->set_priority_called = true;
 
-	if (!node->inherit_rt && is_rt_policy(desired.sched_policy)) {
+	if (!task_is_critical() &&
+	    !node->inherit_rt && is_rt_policy(desired.sched_policy)) {
 		desired.prio = NICE_TO_PRIO(0);
 		desired.sched_policy = SCHED_NORMAL;
 	}
@@ -899,8 +901,11 @@ static void binder_transaction_priority(struct binder_thread *thread,
 	}
 	spin_unlock(&thread->prio_lock);
 
-	binder_set_priority(thread, &desired);
-	trace_android_vh_binder_set_priority(t, task);
+	if (task_is_critical())
+		binder_do_set_priority(thread, &desired, false);
+	else
+		binder_set_priority(thread, &desired);
+		trace_android_vh_binder_set_priority(t, task);
 }
 
 static struct binder_node *binder_get_node_ilocked(struct binder_proc *proc,
