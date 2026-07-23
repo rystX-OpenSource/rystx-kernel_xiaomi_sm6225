f85297707e7a64010ea010cb60c60c6eb100a633
John Galt <johngaltfirstrun@gmail.com>
2026-07-23 09:23:55 -0400

    mm/page_alloc: Give critical tasks faster page allocation paths
    Logic based on android_vh_drain_all_pages_bypass and android_vh_alloc_pages_reclaim_bypass,
    to skip the most expensive operations in the slowpath here (or in case of latter hook, retry
    with additional memory reserve access).
    
    [Tashar02: Backport to msm-5.4 without vendor hooks and s/ALLOC_MIN_RESERVE/ALLOC_HIGH/]
    Change-Id: I915481c0f0dd3399e9a086e3b5a375fb3a09d10b
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>
    Signed-off-by: iDeadXS <datarafi43@gmail.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4253,7 +4253,8 @@ retry:
 	 */
 	if (!page && !drained) {
 		unreserve_highatomic_pageblock(ac, false);
-		drain_all_pages(NULL);
+		if (!task_is_critical())
+			drain_all_pages(NULL);
 		drained = true;
 		goto retry;
 	}
@@ -4670,6 +4671,11 @@ retry:
 	if (fatal_signal_pending(current) && !(gfp_mask & __GFP_NOFAIL) &&
 			(gfp_mask & __GFP_FS))
 		goto nopage;
+
+	if (task_is_critical() && !(alloc_flags & ALLOC_HIGH)) {
+		alloc_flags |= ALLOC_HIGH;
+		goto retry;
+	}
 
 	/* Try direct reclaim and then allocating */
 	if (!used_vmpressure)
