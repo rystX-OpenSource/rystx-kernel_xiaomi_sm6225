5640e57b08795e9d96b717b170731c381153fcbe
iDeadXS <datarafi43@gmail.com>
2026-07-27 21:08:17 +0700

    fs: incfs: sync with newer zstd driver
    Signed-off-by: iDeadXS <datarafi43@gmail.com>

--- fs/incfs/data_mgmt.c
+++ fs/incfs/data_mgmt.c
@@ -432,17 +432,17 @@ static ssize_t zstd_decompress_safe(struct mount_info *mi,
 		return result;
 
 	if (!mi->mi_zstd_stream) {
-		unsigned int workspace_size = ZSTD_DStreamWorkspaceBound(
+		unsigned int workspace_size = zstd_dstream_workspace_bound(
 						INCFS_DATA_FILE_BLOCK_SIZE);
 		void *workspace = kvmalloc(workspace_size, GFP_NOFS);
-		ZSTD_DStream *stream;
+		zstd_dstream *stream;
 
 		if (!workspace) {
 			result = -ENOMEM;
 			goto out;
 		}
 
-		stream = ZSTD_initDStream(INCFS_DATA_FILE_BLOCK_SIZE, workspace,
+		stream = zstd_init_dstream(INCFS_DATA_FILE_BLOCK_SIZE, workspace,
 				  workspace_size);
 		if (!stream) {
 			kvfree(workspace);
