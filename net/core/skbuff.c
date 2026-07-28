6cf2ae7c15275d712a1c0880ac604b5775a09dde
rsuntk <rissu.ntk@gmail.com>
2026-07-28 23:25:29 +0700

    net/skbuff: fix missing semicolon
    Change-Id: I2f0fb484c90abd052ef628cfd72de0dbc567bf3e
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>
    Signed-off-by: iDeadXS <datarafi43@gmail.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -4406,7 +4406,7 @@ static void __skb_complete_tx_timestamp(struct sk_buff *skb,
 	serr->opt_stats = opt_stats;
 	serr->header.h4.iif = skb->dev ? skb->dev->ifindex : 0;
 	if (sk->sk_tsflags & SOF_TIMESTAMPING_OPT_ID) {
-		serr->ee.ee_data = skb_shinfo(skb)->tskey
+		serr->ee.ee_data = skb_shinfo(skb)->tskey;
 		if (sk_is_tcp(sk))
 			serr->ee.ee_data -= atomic_read(&sk->sk_tskey);
 	}
